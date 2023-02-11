7dd2135ff3f18d2f97a8bd6c9556989c5d943320
Reidmen Arostica <r.a.arostica.barrera@rug.nl>
2023-02-11 12:02:45 +0100

    added ichimoku strategy (testing notebook with synthetic data)

--- Coding/Ideas/main.py
+++ Coding/Ideas/main.py
@@ -111,19 +111,51 @@ class Trader:
         # Iterate over all the keys (the available products) contained in the order dephts
         for product in state.order_depths.keys():
 
-            # Check if the current product is the 'PEARLS' product, only then run the order logic
-            if product in ("PEARLS", "BANANAS"):
+            # Run through all the products
+            # Retrieve the Order Depth containing all the market BUY and SELL orders for PEARLS
+            order_depth: OrderDepth = state.order_depths[product]
+            # Initialize the list of Orders to be sent as an empty list
+            orders: list[Order] = []
 
-                # Retrieve the Order Depth containing all the market BUY and SELL orders for PEARLS
-                order_depth: OrderDepth = state.order_depths[product]
-                # Initialize the list of Orders to be sent as an empty list
-                orders: list[Order] = []
+            # Note that this value of 1 is just a dummy value, you should likely change it!
+            acceptable_price = 10000 # self._get_acceptable_price(state, product, bandwidth)
 
-                # Define a fair value for the PEARLS.
-                # Note that this value of 1 is just a dummy value, you should likely change it!
-                bandwidth = 20 if product == "PEARLS" else 5
-                acceptable_price = self._get_acceptable_price(state, product, bandwidth)
+            print(f"state = {state}")
+            print(f"state.own_trades = {state.own_trades}")
+            print(f"state.observations = {state.observations}")
 
+            try:
+                if len(order_depth.sell_orders) > 0:
+                  trades = own_trades[product]
+                  acceptable_buy_price = min([trades[i].price for i in range(len(trades)) if trades[i].buyer == "SUBMISSION"])
+
+
+                  if best_ask < acceptable_buy_price:
+
+                      # Calulating best ask price
+                      best_ask = min(order_depth.sell_orders.keys())
+                      best_ask_volume = order_depth.sell_orders[best_ask]
+
+                      print("BUY", str(-best_ask_volume) + "x", best_ask)
+                      orders.append(Order(product, best_ask, -best_ask_volume))
+
+                if len(order_depth.buy_orders) > 0:
+
+                  # calculating minimum selling price per share (i.e total amount of shares / no. of shares)
+                  acceptable_sell_price = sum([trades[i].price * trades[i].quantity  for i in range(len(trades)) if trades[i].seller== "SUBMISSION"])
+                  acceptable_sell_price /= sum([trades[i].quantity for i in range(len(trades)) if trades[i].seller == "SUBMISSION"])
+                  acceptable_sell_price = math.ceil(acceptable_sell_price)
+
+                  # Calculating best bid price
+                  best_bid = max(order_depth.buy_orders.keys())
+                  best_bid_volume = order_depth.buy_orders[best_bid]
+
+                  # if best bid price is higher than accepted sell price, 
+                  if best_bid > acceptable_sell_price:
+                      print("SELL", str(best_bid_volume) + "x", best_bid)
+                      orders.append(Order(product, best_bid, -best_bid_volume))
+
+            except:
                 # If statement checks if there are any SELL orders in the PEARLS market
                 if len(order_depth.sell_orders) > 0:
 
@@ -154,10 +186,10 @@ class Trader:
                         print("SELL", str(best_bid_volume) + "x", best_bid)
                         orders.append(Order(product, best_bid, -best_bid_volume))
 
-                # Add all the above the orders to the result dict
-                result[product] = orders
+            # Add all the above the orders to the result dict
+            result[product] = orders
 
-                # Return the dict of orders
-                # These possibly contain buy or sell orders for PEARLS
-                # Depending on the logic above
+            # Return the dict of orders
+            # These possibly contain buy or sell orders for PEARLS
+            # Depending on the logic above
         return result
