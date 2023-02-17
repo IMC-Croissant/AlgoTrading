36c2e8f682d76e678fde9b6c09e124a78b08177a
Andrew Chan <66477660+andrewchan868@users.noreply.github.com>
2023-02-18 00:45:37 +0800

    Update T_Vwap.py

--- Coding/Ideas/T_Vwap.py
+++ Coding/Ideas/T_Vwap.py
@@ -1,49 +1,29 @@
-from typing import List, Dict
-from collections import defaultdict
-from datamodel import Trade
+from typing import Dict, List
+from datamodel import Listing, OrderDepth, Trade, TradingState, Order
 
-def calculate_vwap(trades: List[Trade]) -> Dict[str, float]:
-    vwap_by_product = defaultdict(float)
-    volume_by_product = defaultdict(int)
+def vwap(trades: List[Trade], volume: int) -> float:
+    trade_sum = 0
+    volume_remaining = volume
     for trade in trades:
-        vwap_by_product[trade.symbol] += trade.price * trade.quantity
-        volume_by_product[trade.symbol] += trade.quantity
-    for product in vwap_by_product:
-        vwap_by_product[product] /= volume_by_product[product]
-    return dict(vwap_by_product)
+        if trade.quantity <= volume_remaining:
+            trade_sum += trade.price * trade.quantity
+            volume_remaining -= trade.quantity
+        else:
+            trade_sum += trade.price * volume_remaining
+            break
+    return trade_sum / volume
 
-from typing import List
-from datamodel import Trade
-
-def calculate_twap(trades: List[Trade], start_time: int, end_time: int) -> float:
-    total_value = 0.0
-    total_volume = 0.0
-    last_trade_time = start_time
+def twap(trades: List[Trade], volume: int, time_delta: int) -> float:
+    trade_sum = 0
+    volume_remaining = volume
+    time_elapsed = 0
     for trade in trades:
-        # If the trade occurred outside the specified time range, skip it
-        if trade.timestamp < start_time or trade.timestamp > end_time:
-            continue
-        
-        # Calculate the time interval since the last trade
-        time_interval = trade.timestamp - last_trade_time
-        
-        # Add the value of this trade to the running total
-        total_value += trade.price * trade.quantity
-        
-        # Add the volume of this trade to the running total
-        total_volume += trade.quantity
-        
-        # Update the last trade time
-        last_trade_time = trade.timestamp
-    
-    # Calculate the total time interval
-    total_time = end_time - start_time
-    
-    # Calculate the TWAP
-    if total_volume == 0 or total_time == 0:
-        twap = 0
-    else:
-        twap = total_value / total_volume
-    
-    return twap
-
+        if trade.quantity <= volume_remaining:
+            trade_sum += trade.price * trade.quantity
+            volume_remaining -= trade.quantity
+            time_elapsed += (trade.timestamp - time_elapsed) * trade.quantity / volume
+        else:
+            trade_sum += trade.price * volume_remaining
+            time_elapsed += (trade.timestamp - time_elapsed) * volume_remaining / volume
+            break
+    return trade_sum / volume
