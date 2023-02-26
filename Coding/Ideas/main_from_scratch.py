062ba1ecac6109154851d5a7a74a8a1ea300cb42
Reidmen Arostica <r.a.arostica.barrera@rug.nl>
2023-02-26 12:30:42 +0100

    Added unit test, datamodel and main_from_scratch script for basic usage

--- Coding/Ideas/main_from_scratch.py
+++ Coding/Ideas/main_from_scratch.py
@@ -1,59 +1,80 @@
 from typing import Dict, List
 from datamodel import OrderDepth, TradingState, Order
 from pandas import DataFrame
+from numpy import nan
 
 
 class Trader:
     _history = DataFrame({
-        'PEARLS': 9980,
-        'BANANAS': 4500,
+        'PEARLS': [10000],
+        'BANANAS': [10000],
             })
 
-    def _get_acceptable_price(self, state: TradingState, product: str):
+    def _get_acceptable_price(self, state: TradingState, product: str) -> tuple:
         """Computes acceptable price from historical data. """
         history_product = self._history[product]
+        # print("history_product \n", history_product)
         # compute rolling mean of size 20 if possible
-        print("state timestamp ", state.timestamp)
-        if state.timestamp > 2000:
-            history_product.rolling(window=10).mean()
+        # print("state timestamp ", state.timestamp)
+        if state.timestamp > 1500:
+            history_rolling = history_product.rolling(window=10)
+            means = history_rolling.mean()
+            stds = history_rolling.std()
+            # fill na's 
+            means = means.fillna(history_product.mean())
+            stds = stds.fillna(0)
+            # if means.isnull().values.any():
+            #     acceptable_price = history_product.iloc[0]
+            #     std = 0.0
+            acceptable_price = means.mean()
+            std = stds.mean()
+
         else:
             acceptable_price = history_product.mean()
+            std = 0.0
 
-        return acceptable_price
+        print("computed acceptable price {} std {} for {}".format(
+            acceptable_price, std, product))
+
+        return acceptable_price, std
 
     def _process_new_data(self, state: TradingState) -> None:
         """Adds new data point to historical data."""
         # initialize values at the beginning
         # own_trades still not populated
         if ("PEARLS" not in state.own_trades.keys() or "BANANAS" not in state.own_trades.keys()):
-            self._history.append({
-                "PEARLS": 9980,
-                "BANANAS": 4890,
+            pass
+        else:
+            # own_trades[product] is a Trade object
+            # it has attributed (symbol, price, quantity, buyer, seller)
+            own_trades = state.own_trades
+            # getting average price per share
+            avg_pearls = sum([trade.price * trade.quantity for trade in own_trades["PEARLS"]])
+            avg_pearls /= sum([trade.quantity for trade in own_trades["PEARLS"]])
+            avg_bananas = sum([trade.price * trade.quantity for trade in own_trades["BANANAS"]])
+            avg_bananas /= sum([trade.quantity for trade in own_trades["BANANAS"]])
+
+            print("avg_bananas, ", avg_bananas)
+            print("avg_pearls, ", avg_pearls)
+            self._history = self._history.append({
+                "PEARLS": avg_pearls,
+                "BANANAS": avg_bananas,
                 },
                 ignore_index=True)
-        else:
-            # own_trades[product] is a tuple (product, << SUBMISSION, price, quantity)
-            own_trades = state.own_trades
-            self._history.append({
-                "PEARLS": own_trades["PEARLS"][2],
-                "BANANAS": own_trades["BANANAS"][2]
-                })
 
     def run(self, state: TradingState) -> Dict[str, List[Order]]:
         """
         Only method required. It takes all buy and sell orders for all symbols as an input,
         and outputs a list of orders to be sent
         """
-        # get history
-        # self._history.append(state.timestamp)
-        # print("history size ", len(self._history))
         # Initialize the method output dict as an empty dict
         result = {}
 
         print("current state own orders ", state.own_trades)
-        print("current state observation ", state.observations)
+        # print("current state observation ", state.observations)
         # process new data
         self._process_new_data(state)
+        # print("current data ", self._history)
 
         # Iterate over all the keys (the available products) contained in the order depths
         for product in state.order_depths.keys():
@@ -70,7 +91,7 @@ class Trader:
                 # Define a fair value for the PEARLS.
                 # Note that this value of 1 is just a dummy value, you should likely change it!
                 # acceptable_price = 4900 if product == 'PEARLS' else 9980
-                acceptable_price = self._get_acceptable_price(state, product)
+                acceptable_price, std = self._get_acceptable_price(state, product)
                 # If statement checks if there are any SELL orders in the PEARLS market
                 if len(order_depth.sell_orders) > 0:
 
@@ -80,7 +101,7 @@ class Trader:
                     best_ask_volume = order_depth.sell_orders[best_ask]
 
                     # Check if the lowest ask (sell order) is lower than the above defined fair value
-                    if best_ask < acceptable_price:
+                    if best_ask < acceptable_price - 2*std:
 
                         # In case the lowest ask is lower than our fair value,
                         # This presents an opportunity for us to buy cheaply
@@ -97,7 +118,7 @@ class Trader:
                 if len(order_depth.buy_orders) != 0:
                     best_bid = max(order_depth.buy_orders.keys())
                     best_bid_volume = order_depth.buy_orders[best_bid]
-                    if best_bid > acceptable_price:
+                    if best_bid > acceptable_price + 2*std:
                         print("SELL", str(best_bid_volume) + "x", best_bid)
                         orders.append(Order(product, best_bid, -best_bid_volume))
 
---
+++ Coding/Ideas/unit_test.py
@@ -0,0 +1,85 @@
+from datamodel import Listing, OrderDepth, Trade, TradingState
+from main_from_scratch import Trader
+import time
+
+timestamp = 2100
+
+listings = {
+	"PEARLS": Listing(
+		symbol="PEARLS",
+		product="PEARLS",
+		denomination= "SEASHELLS",
+	),
+	"BANANAS": Listing(
+		symbol="BANANAS",
+		product="BANANAS",
+		denomination="SEASHELLS",
+	),
+}
+
+order_depths = {
+	"PEARLS": OrderDepth(
+		buy_orders={10: 7, 9: 5},
+		sell_orders={12: -5, 13: -3}
+	),
+	"BANANAS": OrderDepth(
+		buy_orders={142: 3, 141: 5},
+		sell_orders={144: -5, 145: -8}
+	),
+}
+
+own_trades = {
+	"PEARLS": [
+		Trade(
+			symbol="PEARLS",
+			price=11,
+			quantity=4,
+			buyer="SUBMISSION",
+			seller="",
+		),
+		Trade(
+			symbol="PEARLS",
+			price=12,
+			quantity=3,
+			buyer="SUBMISSION",
+			seller="",
+		)
+	],
+	"BANANAS": [
+		Trade(
+			symbol="BANANAS",
+			price=143,
+			quantity=2,
+			buyer="",
+			seller="SUBMISSION",
+		),
+	]
+}
+
+market_trades = {
+	"PEARLS": [],
+	"BANANAS": []
+}
+
+position = {
+	"PEARLS": 3,
+	"BANANAS": -5
+}
+
+observations = {}
+
+state = TradingState(
+	timestamp,
+        listings,
+	order_depths,
+	own_trades,
+	market_trades,
+        position,
+        observations,
+)
+
+start_time = time.time()
+trader = Trader()
+trader.run(state)
+elapsed_time = time.time() - start_time
+print("total time for an iteration {} [s]".format(elapsed_time))
